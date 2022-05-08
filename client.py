aaf84799b160c4e2001637493ac0de2c0afeb8e9
nihadenes <91975592+nihadenes@users.noreply.github.com>
2022-05-08 13:09:30 +0300

    Add files via upload

--- client.py
+++ client.py
@@ -27,7 +27,51 @@ from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from cryptography.fernet import Fernet
-from PIL import Image
+from datetime import datetime
+
+
+
+# Config area.
+
+global SERVER, PORT, FORMAT, PREFIX, HEADER, ADDR
+SERVER, PORT, FORMAT, HEADER = socket.gethostbyname(socket.gethostname()), 9090, "utf-8", 64
+ADDR = (SERVER, PORT)
+
+PREFIX = {
+    "error": "[!]",
+    "info": "[*]",
+    "warning": "[-]",
+}
+
+log_messages = {
+    "server.client.connect": "[{ip}][{port}] Connected.",
+    "server.client.disconnect": "[{ip}][{port}] Disconnected.",
+    "server.client.client.message": "[{ip}][{port}] [{packet}] [{msg}]",
+    "server.client.request": "[{ip}][{port}] Requesting [{msg}]",
+    "server.client.getkey": "[{ip}][{port}] Requests key, the key is [{msg}]",
+    "server.start": "Server is starting...",
+    "server.listen": "Server is listening on {ip};"
+}
+
+
+# 100-199 are for client side, 200-299 are for server side, and 300-399 are for general side errors..
+
+error_messages = {
+    100: "Client sent corrupt packet.",
+    101: "Client got disconnected.",
+    102: "Client sent a request that is not supported.",
+    103: "Client sent so many fucking packets.",
+
+    200: "Server error.",
+    201: "Server sent corrupt packet.",
+    202: "Server got disconnected.",
+
+    300: "Corrupt packet.",
+    301: "Connection lost.",
+    302: "Connection failed.",
+    303: "Hash is incorrect.",
+    304: "Message is corrupted."
+}
 
 
 # Functions for encrypting and hashing.
@@ -66,14 +110,14 @@ def hash_sha256(hash_string):
 def fernetencrypt(key, string):
     try:
         return Fernet(key.encode("utf-8")).encrypt(string.encode("utf-8")).decode("utf-8")
-    except Exception as e:
+    except:
         return False
 
 
 def fernetdecrypt(key, string):
     try:
         return Fernet(key.encode("utf-8")).decrypt(string.encode("utf-8")).decode("utf-8")
-    except Exception as e:
+    except:
         return False
 
 
@@ -96,54 +140,88 @@ def paragraph(string, liner):
 
 # Socket functions.
 
-def getenc(packetype=None, message=None, ipadress=None):
-    return json.dumps({"packagetype": packetype, "message": message, "ipadress": ipadress})
+def getenc(packagetype=None, message=None, ipadress=None, key=None):
+    message = json.dumps({"packagetype": packagetype, "message": message, "ipadress": ipadress})
+    if key != None:
+        return encode64(encode64("encrypted.packet") + "_" + fernetencrypt(key, json.dumps(message)))
+    else:
+        return encode64(encode64(hash_sha256(message)) + "_" + encode64(message))
 
 
-def getdec(msg):
-    return json.loads(msg)
-
-
-def send(msg, client):
-    message = encode64(msg).encode(FORMAT)
-    send_length = str(len(message)).encode(FORMAT) + b' ' * (HEADER - len(str(len(message)).encode(FORMAT)))
-    client.send(send_length)
-    client.send(message)
-    return client.recv(2048).decode(FORMAT)
-
-
-def newclient():
+def getdec(msg, key=None):
     try:
-        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        client.connect(ADDR)
-        print(send(getenc(packetype="client.message", message=randomlet(16)), client))
-        print(send(getenc(packetype="client.disconnect", message=randomlet(16)), client))
-        del client
+        if key != None:
+            msg = fernetdecrypt(key, msg)
+            return json.loads(msg)
+        else:
+            msg = decode64(msg)
+            hash, msg = decode64(msg.split("_")[0]), decode64(msg.split("_")[1])
+            if hash == hash_sha256(msg):
+                return json.loads(msg)
+            else:
+                raise Exception(303)
+    except:
+        raise Exception(304)
+
+
+def console_log(ip=None, port=None, cpacket=None, packet=None, msg=None, prefix="info"):
+    date = "["  + datetime.today().strftime('%Y-%m-%d') + "] [" + datetime.today().strftime('%H:%M:%S') + "]"
+    end = PREFIX[prefix] + " " + date + " " + log_messages[cpacket].format(prefix=prefix, ip=ip, port=port, packet=packet, msg=msg)
+    print(end)
+
+
+def send(client=None, packagetype=None, message=None, ipadress=None):
+    def directsend(client=client, packagetype=packagetype, message=message, ipadress=ipadress):
+
+        message = getenc(packagetype=packagetype, message=message, ipadress=ipadress)
+        send_length = str(len(message.encode(FORMAT))).encode(FORMAT) + b' ' * (HEADER - len(str(len(message.encode(FORMAT))).encode(FORMAT)))
+
+        try:
+            client.send(send_length)
+            client.send(message.encode(FORMAT))
+        except:
+            return "connection_error"
+
+        try:
+            receive = client.recv(2048).decode(FORMAT)
+        except:
+            return "connection_error"
+
+        try:
+            return getdec(receive)
+        except:
+            return "message_corrupted"
+
+    tries = 9999999
+    while tries > 0:
+        tries -= 1 if tries != 0 else 0
+        result = directsend()
+        if result == "connection_error" or result == "client_side_error":
+            raise Exception(f"Connection error, {result}.")
+        elif result == "message_corrupted":
+            pass
+        else:
+            return result
+    return False
+
+
+def client():
+
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect(ADDR)
+
+    hash_key = send(client=client, packagetype="client.getkey")
+    print(hash_key["message"])
+
+
+main = True
+errortry = 5
+while main:
+    try:
+        client()
+        main = False
     except Exception as e:
-        print(e)
-        return False
-
-
-SERVER, PORT, FORMAT, PREFIX, HEADER = "sussyip", 8080, "utf-8", "[INFO]", 64
-ADDR = (SERVER, PORT)
-
-# try:
-# while True:
-# thread = threading.Thread(target=newclient, args=())
-# thread.start()
-# time.sleep(3)
-# except Exception as e:
-#     print(f"An error occured, {e}")
-#     input()
-#193.164.7.174
-
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(ADDR)
-
-output2 = [i[2:-2] for i in [str(item.split("\r")[:-1]) for item in subprocess.check_output(['systeminfo']).decode('utf-8').split('\n')]]
-
-
-print(send(getenc(packetype="client.message", message="\n".join(output2)), client))
-print(send(getenc(packetype="client.disconnect"), client))
-
-
+        time.sleep(300) if errortry == 0 else None
+        errortry -= 1 if errortry != 0 else 0
+        print(f"Client failed, [{e}], [{e.__class__}].")
+        time.sleep(5)
--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 # ------------------------- [ Sub Project File | Coding: utf-8 ] -------------------------- #
-# Project: SocketServer                                                                     #
+# Project: SocketClient                                                                     #
 # File: main.py	                                                                            #
 # Python Version: 3.10.2 - Tested: 3.10.2 - All others are untested.                        #
 # The libraries should get installed among the integrated libraries: Libraries			    #
@@ -12,12 +12,14 @@
 # License: License																			#
 # --------------------------------------- [ Enjoy ] --------------------------------------- #
 
+import subprocess
 import threading
 import hashlib
 import random
 import string
 import base64
 import socket
+import time
 import json
 import re
 
@@ -25,8 +27,51 @@ from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from cryptography.fernet import Fernet
-from PIL import Image
+from datetime import datetime
 
+
+
+# Config area.
+
+global SERVER, PORT, FORMAT, PREFIX, HEADER, ADDR
+SERVER, PORT, FORMAT, HEADER = socket.gethostbyname(socket.gethostname()), 9090, "utf-8", 64
+ADDR = (SERVER, PORT)
+
+PREFIX = {
+    "error": "[!]",
+    "info": "[*]",
+    "warning": "[-]",
+}
+
+log_messages = {
+    "server.client.connect": "[{ip}][{port}] Connected.",
+    "server.client.disconnect": "[{ip}][{port}] Disconnected.",
+    "server.client.client.message": "[{ip}][{port}] [{packet}] [{msg}]",
+    "server.client.request": "[{ip}][{port}] Requesting [{msg}]",
+    "server.client.getkey": "[{ip}][{port}] Requests key, the key is [{msg}]",
+    "server.start": "Server is starting...",
+    "server.listen": "Server is listening on {ip};"
+}
+
+
+
+
+# 100-199 are for client side, 200-299 are for server side, and 300-399 are for general side errors..
+
+error_messages = {
+    100: "Client sent corrupt packet.",
+    101: "Client got disconnected.",
+    102: "Client sent a request that is not supported.",
+    103: "Client sent so many fucking packets.",
+    
+    200: "Server error.",
+    201: "Server sent corrupt packet.",
+    202: "Server got disconnected.",
+    
+    300: "Corrupt packet.",
+    301: "Connection lost.",
+    302: "Connection failed.",
+}
 
 
 # Functions for encrypting and hashing.
@@ -65,82 +110,141 @@ def hash_sha256(hash_string):
 def fernetencrypt(key, string):
     try:
         return Fernet(key.encode("utf-8")).encrypt(string.encode("utf-8")).decode("utf-8")
-    except Exception as e:
+    except:
         return False
 
 
 def fernetdecrypt(key, string):
     try:
         return Fernet(key.encode("utf-8")).decrypt(string.encode("utf-8")).decode("utf-8")
-    except Exception as e:
+    except:
         return False
-
+    
 
 def fernetgetkey(password, salt):
     return base64.urlsafe_b64encode(
-        PBKDF2HMAC(algorithm=hashes.SHA256(), length=32, salt=hash_sha256(hash_sha256(salt)).encode("utf-8"), iterations=100000,
+        PBKDF2HMAC(algorithm=hashes.SHA256(), length=32, salt=hash_sha256(hash_sha256(salt)).encode("utf-8"),
+                   iterations=100000,
                    backend=default_backend()).derive(hash_sha256(hash_sha256(password)).encode())).decode("utf-8")
+
+
+# Useful functions.
+
+def randomlet(lentgth):
+    return ''.join(random.choice(string.ascii_letters) for i in range(lentgth))
+
+
+def paragraph(string, liner):
+    return [string[i:i + liner] for i in range(0, len(list(string)), liner)]
 
 
 # Socket functions.
 
-def getenc(packettype=None, message=None, ipadress=None):
-    return '("' + '", "'.join([encode64(str(x)) for x in [packettype, message, ipadress]]) + '")'
+def getenc(packagetype=None, message=None, ipadress=None):
+    message = json.dumps({"packagetype": packagetype, "message": message, "ipadress": ipadress})
+    return encode64(encode64(hash_sha256(message)) + "_" + encode64(message))
 
-
+ 
 def getdec(msg):
-    return [decode64(x) for x in msg.split('"')[1::2]]
+    try:
+        msg = decode64(msg)
+        hash, msg = decode64(msg.split("_")[0]), decode64(msg.split("_")[1])
+        if hash == hash_sha256(msg):
+            return json.loads(msg)
+        else:
+            raise Exception("Hash incorrect.")
+    except:
+        raise Exception("Message corrupted.")
 
 
-def console_log(ip=None, port=None, cpacket=None, packet=None, msg=None, prefix=None):
-    data = json.load(open("console_msg.json", "r") )
-    end = data[cpacket].format(prefix=prefix, ip=ip, port=port, packet=packet, msg=msg) if cpacket in data else False
+def console_log(ip=None, port=None, cpacket=None, packet=None, msg=None, prefix="info"):
+    date = "["  + datetime.today().strftime('%Y-%m-%d') + "] [" + datetime.today().strftime('%H:%M:%S') + "]"
+    end = PREFIX[prefix] + " " + date + " " + log_messages[cpacket].format(prefix=prefix, ip=ip, port=port, packet=packet, msg=msg)
     print(end)
-    return end
-    
+
 
 def handle_client(conn, addr):
-    console_log(ip=addr[0], port=addr[1], cpacket="client.connect", prefix=PREFIX)
+    def directsend(conn=conn):
+        try:
+            msg_length = conn.recv(HEADER).decode(FORMAT)
+        except:
+            return "connection.lost"
 
+        if msg_length:
+            try:
+                msg_length = int(msg_length)
+            except:
+                return "message.corrupted"
+            try:
+                receive = conn.recv(msg_length).decode(FORMAT)
+            except:
+                return "connection.lost"
+            try:
+                receive = getdec(receive)
+            except:
+                return "message.corrupted"
+            return receive
+
+
+    console_log(ip=addr[0], port=addr[1], cpacket="server.client.connect")
+    reply = None
     connected = True
     while connected:
-        msg_length = conn.recv(HEADER).decode(FORMAT)
-        if msg_length:
-            msg_length = int(msg_length)
-            try:
-                msg = json.loads(decode64(conn.recv(msg_length).decode(FORMAT)))
-            except Exception as e:
-                conn.send(f"message.corrupted {e}".encode(FORMAT))
+        msg = directsend()
+        if msg:
+            if msg == "connection.lost":
+                console_log(ip=addr[0], port=addr[1], cpacket="server.client.disconnect")
                 connected = False
 
-            if msg["packagetype"] == "client.disconnect":
-                console_log(ip=addr[0], port=addr[1], cpacket="client.disconnect", prefix=PREFIX)
-                connected = False
+            elif msg == "message.corrupted":
+                console_log(ip=addr[0], port=addr[1], cpacket="server.client.corrupt")
+                reply = getenc(packagetype="message.corrupted")
+
             else:
-                console_log(ip=addr[0], port=addr[1], cpacket="client.message", packet="Message", msg=msg,  prefix=PREFIX)
-                
-            conn.send("msg.received".encode(FORMAT))
 
-def start():
+
+                if msg["packagetype"] == "client.disconnect":
+                    console_log(ip=addr[0], port=addr[1], cpacket="server.client.disconnect")
+                    connected = False
+
+
+                elif msg["packagetype"] == "client.getkey":
+                    hash_key = fernetgetkey(hash_sha256(randomlet(128)), hash_sha256(randomlet(128)))
+                    reply = getenc(packagetype="server.hashkey", message=hash_key)
+                    console_log(ip=addr[0], port=addr[1], cpacket="server.client.getkey", msg=hash_key)
+
+                else:
+                    console_log(ip=addr[0], port=addr[1], cpacket="server.client.message", packet="Message", msg=msg)
+
+        if reply != None:
+            conn.send(reply.encode(FORMAT))
+            reply = None
+
+
+def server():
+    
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.settimeout(10.0)
+    server.bind(ADDR)
+
+    console_log(cpacket="server.start")
     server.listen()
-    console_log(cpacket="server.listen", ip=SERVER, prefix=PREFIX)
+    console_log(cpacket="server.listen", ip=SERVER)
     while True:
         conn, addr = server.accept()
         thread = threading.Thread(target=handle_client, args=(conn, addr))
         thread.start()
+        
 
-
-try:
-    SERVER, PORT, FORMAT, PREFIX, HEADER = socket.gethostbyname(socket.gethostname()), 8080, "utf-8", "[INFO]", 64
-    ADDR = (SERVER, PORT)
-
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind(ADDR)
-
-    console_log(cpacket="server.start", prefix=PREFIX)
-    start()
-    
-    
-except Exception as e:
-    print(f"An error occured, {e}")
-    input()
+main = True
+errortry = 5
+while main:
+    try:
+        server()
+        main = False
+    except Exception as e:
+        time.sleep(300) if errortry == 0 else None
+        errortry -= 1 if errortry != 0 else 0
+        console_log(cpacket="server.error")
+        time.sleep(5)
+        
